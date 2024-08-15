1a0e08b702e009a510bdffe6f02fc65f937e08d2
murilo.lima <murilossl07@gmail.com>
2024-08-15 11:36:54 -0300

    Aula 01 POO

---
+++ src/Pessoa.ts
@@ -0,0 +1,39 @@
+export class Pessoa{
+
+    //ATRIBUTOS
+    private nome: string;
+    private idade: number;
+
+    //CONSTRUTOR
+    public constructor(_nome: string, _idade: number){
+        this.nome = _nome;
+        this.idade = _idade
+    }
+
+    //METODOS GET E SET
+    public getNome() : string {
+        return this.nome;
+    }
+    public setNome(_nome: string): void {
+        this.nome = _nome;
+    }
+    public getIdade(): number {
+        return this.idade;
+    }
+    public setIdade(_idade: number): void {
+        this.idade = _idade;
+    }
+
+    //METODOS DIAGRAMA
+    public idadePessoa(): number {
+        return this.idade;
+    }
+    public aniversario(): number {
+        this.idade++;
+        return this.idade;
+    }
+
+    public correr(): void {
+        console.log(`${this.nome} está correndo... RUN ${this.nome} RUN`);
+    }
+}
